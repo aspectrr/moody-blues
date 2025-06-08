0bc9e167f33a852ceb1fc5d285ce15c30d9d2a1c
Collin Pfeifer <collinpfeifer@Icloud.com>
2025-06-08 09:07:33 -0700

    feat: update docker-compose file for dev

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  dialect: "postgresql", // 'mysql' | 'sqlite' | 'turso'
+  dialect: "postgresql",
   schema: "./src/db/schema.ts",
 });
